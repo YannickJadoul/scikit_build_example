2c3480a01e971b1d6b460f6287d0215fee088ca1
Yannick Jadoul <yannick.jadoul@belgacom.net>
2021-01-03 00:44:55 +0100

    Try out YannickJadoul/scikit-build@vs2019

--- setup.py
+++ setup.py
@@ -14,6 +14,32 @@ except ImportError:
     )
     raise
 
+
+def patched_WindowsPlatform_init(self):
+	import textwrap
+	from skbuild.platform_specifics.windows import WindowsPlatform, CMakeVisualStudioCommandLineGenerator, CMakeVisualStudioIDEGenerator
+
+	super(WindowsPlatform, self).__init__()
+
+	self._vs_help = textwrap.dedent("""
+		Building Windows wheels for requires Microsoft Visual Studio 2017 or 2019:
+
+		  https://visualstudio.microsoft.com/vs/
+		""").strip()
+
+	supported_vs_years = [("2019", "v141"), ("2017", "v141")]
+	for vs_year, vs_toolset in supported_vs_years:
+		self.default_generators.extend([
+			CMakeVisualStudioCommandLineGenerator("Ninja", vs_year, vs_toolset),
+			CMakeVisualStudioIDEGenerator(vs_year, vs_toolset),
+			CMakeVisualStudioCommandLineGenerator("NMake Makefiles", vs_year, vs_toolset),
+			CMakeVisualStudioCommandLineGenerator("NMake Makefiles JOM", vs_year, vs_toolset)
+		])
+
+import skbuild.platform_specifics.windows
+skbuild.platform_specifics.windows.WindowsPlatform.__init__ = patched_WindowsPlatform_init
+
+
 setup(
     name="scikit_build_example",
     version="0.0.1",
